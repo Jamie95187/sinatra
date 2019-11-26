f66cbfcdb673f5b25c3f3208133565bed5b2fb08
Student <student@Makerss-MacBook-Air.local>
2019-11-26 15:33:25 +0000

    add views directory and index file for images

--- lib/app.rb
+++ lib/app.rb
@@ -12,8 +12,6 @@ get '/manual' do
   "Hey There Manual"
 end
 
-get '/monkey' do
-  "<div style='border :2px dashed red'>
-    <img src = 'https://images.unsplash.com/photo-1540573133985-87b6da6d54a9?ixlib=rb-1.2.1&dpr=1&auto=format&fit=crop&w=416&h=312&q=60'>
-  </div>"
+get '/cat' do
+  erb(:index)
 end
